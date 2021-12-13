7b2e8abd113a4445126c75c88af39b66e2b0727e
Tim Kravel <TKravel@Yahoo.com>
2021-12-12 20:13:20 -0500

    Set up port for heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
